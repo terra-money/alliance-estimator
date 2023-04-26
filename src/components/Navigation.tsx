cc5f7690b43a68b969a7b78b98a9b6eee8a1ff87
Joshua <joshua.brigati@gmail.com>
2023-04-26 14:11:21 -0500

    branches have been merged together, bugs fixed and other small UI changes

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,28 @@
-import { useState } from 'react';
+import { useLocation } from 'react-router-dom';
 import styles from "styles/Navigation.module.scss";
 
-const Navigation = () => {
-  const [hoveredIndex, setHoveredIndex] = useState(-1);
+const Navigation = ({ handleModalOpen }: { handleModalOpen: () => void }) => {
+  const location = useLocation();
+
+  const shareIcon = (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="20"
+      height="20"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+    >
+      <circle cx="18" cy="5" r="3"></circle>
+      <circle cx="6" cy="12" r="3"></circle>
+      <circle cx="18" cy="19" r="3"></circle>
+      <line x1="8.59" y1="13.51" x2="15.42" y2="17.49"></line>
+      <line x1="15.41" y1="6.51" x2="8.59" y2="10.49"></line>
+    </svg>
+  );
 
   return (
     <nav className={styles.navigation}>
@@ -20,19 +40,55 @@ const Navigation = () => {
           />
           <h3>Alliance Estimator</h3>
         </a>
+        {location.pathname !== "/mock-data" && (
+          <button onClick={handleModalOpen}>{shareIcon}</button>
+        )}
       </div>
       <div className={styles.button_container}>
+        {location.pathname !== "/mock-data" ? (
+          <div>
+            <a
+              className={styles.mockDataLink}
+              href="/mock-data"
+            >
+              Mock data
+              <img
+                src="/Icons/Arrow.svg"
+                alt="Arrow"
+                width={14}
+                height={14}
+                className={styles.arrow}
+              />
+            </a>
+          </div>
+        ) : (
+          <div>
+            <a
+              className={styles.mockDataLink}
+              href="/"
+            >
+              Back to estimator
+              <img
+                src="/Icons/Arrow.svg"
+                alt="Arrow"
+                width={14}
+                height={14}
+                className={styles.arrow}
+              />
+            </a>
+          </div>
+        )}
         <a
-          className={`${styles.terra_link} ${hoveredIndex === 0 ? styles.hovered : ''}`}
+          className={styles.terra_link}
           href='https://www.terra.money/'
-          onMouseOver={() => setHoveredIndex(0)}
-          onMouseLeave={() => setHoveredIndex(-1)}
           target="_blank"
           rel="noopener noreferrer"
         >
           <img
             src="/Icons/TerraLogo.svg"
             alt="Terra"
+            width={16}
+            height={16}
           />
           <span>terra.money</span>
           <img
@@ -43,16 +99,17 @@ const Navigation = () => {
             className={styles.arrow}
           />
         </a>
-        <button
-          className={`${styles.terra_link} ${styles.documentation} ${hoveredIndex === 2 ? styles.hovered : ''}`}
-          onMouseOver={() => setHoveredIndex(2)}
-          onMouseLeave={() => setHoveredIndex(-1)}
+        <a
+          className={styles.terra_link}
+          href='https://alliance.terra.money/'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           <img
             src="/Icons/Docs.svg"
             alt="Documentation Icon"
           />
-          <span>Documentation</span>
+          <span>Docs</span>
           <img
             src="/Icons/Arrow.svg"
             alt="Arrow"
@@ -60,7 +117,7 @@ const Navigation = () => {
             height={14}
             className={styles.arrow}
           />
-        </button>
+        </a>
       </div>
     </nav>
   )
